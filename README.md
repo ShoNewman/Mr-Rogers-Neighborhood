0bc62570ecd02d41e18b89dde82d83ce5d631d75
Shoshana Newman <sho.newman@gmail.com>
2021-06-11 09:09:02 -0700

    Test for user input of 0 to return an array with element of 0

---
+++ README.md
@@ -0,0 +1,3 @@
+Describe: beepBoop()
+Test: "It should return an array with a 0 if the number 0 is inputted"
+Expected Output: beepBoopArray = [0];
---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+let beepBoopArray = [];
+function beepBoop(userInput) {
+  if (userInput === 0) {
+    return beepBoopArray.push(userInput)
+  }
+}
+beepBoop(0)
+console.log(beepBoop);
